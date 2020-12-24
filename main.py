6c43c2c791cfe167d8c99179993a6eae7f8b7067
wictor-parmenis <wictortec@gmail.com>
2020-12-24 19:09:25 -0300

    created the class what do passwords

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import sqlite3
 import random
 import string
+from tkinter import *
 
 class Password:
 
     password = []
     letters = string.ascii_lowercase
+    new_pwd = ''
 
     def __init__(self, number_char, context):
-        self._number_char = int(number_char / 2)
+        self._number_char = int(number_char)
         self._context = context
 
     def add_password(self):
         # Falta gerar condicional para caso o number_char seja par ou ímpar.
         for i in range(self._number_char):
-            self.password.append(random.choice(self.letters))
-            self.password.append(random.randint(0, 9))
-        return print(self.password)
-
+            if i % 2 == 0:
+                self.password.append(random.choice(self.letters))
+            elif i % 2 == 1:
+                self.password.append(random.randint(0, 9))  
+        #self.password = str(self.password)  
+        print(f'Password with {self._number_char} characters for {self._context}: ', end='')    
+        for text in self.password:
+            print(f'{text}', end='')
+        
+class GUI:
+    
 
 
 if __name__ == '__main__':
 
-    a1 = Password(3, 'instagram')
+    a1 = Password(11, 'instagram')
     a1.add_password()
 
